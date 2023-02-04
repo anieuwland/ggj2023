b5047f5c3e787fe6af074e4616d0cc0d03d31c46
Aart <aart_odding@hotmail.com>
2023-02-04 11:53:22 +0100

    Can choose characters

--- Menus/CharacterSelection.gd
+++ Menus/CharacterSelection.gd
@@ -6,8 +6,21 @@ extends CenterContainer
 # var b = "text"
 
 var player1_character = 0
-var player2_character = 1
+var player2_character = 3
 var characters = []
+
+
+func update_player1():
+		var n = find_node("Player1")
+		while n.get_child_count() > 0:
+			n.remove_child(n.get_child(0))
+		n.add_child(characters[player1_character].duplicate())
+
+func update_player2():
+		var n = find_node("Player2")
+		while n.get_child_count() > 0:
+			n.remove_child(n.get_child(0))
+		n.add_child(characters[player2_character].duplicate())
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,6 +29,8 @@ func _ready():
 	characters.append(find_node("Character3").get_child(0))
 	characters.append(find_node("Character4").get_child(0))
 	characters.append(find_node("Character4").get_child(0))
+	update_player1()
+	update_player2()
 	pass # Replace with function body.
 
 
@@ -29,6 +44,8 @@ func _process(delta):
 			player1_character -= 1
 		if player1_character < 0:
 			player1_character = initial_value
+		else:
+			update_player1()
 
 	if Input.is_action_just_pressed("player1_right"):
 		var initial_value = player1_character
@@ -37,6 +54,8 @@ func _process(delta):
 			player1_character += 1
 		if player1_character > 3:
 			player1_character = initial_value
+		else:
+			update_player1()
 	
 	if Input.is_action_just_pressed("player2_left"):
 		var initial_value = player2_character
@@ -45,6 +64,8 @@ func _process(delta):
 			player2_character -= 1
 		if player2_character < 0:
 			player2_character = initial_value
+		else:
+			update_player2()
 
 	if Input.is_action_just_pressed("player2_right"):
 		var initial_value = player2_character
@@ -53,10 +74,12 @@ func _process(delta):
 			player2_character += 1
 		if player2_character > 3:
 			player2_character = initial_value
+		else:
+			update_player2()
 	
 	
 	
-	print(player1_character, player2_character)
+	#print(player1_character, player2_character)
 	
 	pass
 
