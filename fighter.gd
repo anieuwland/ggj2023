14f88eb05712b9b61f5c0f6cc7d6626d9c1bdfa7
Aart Stuurman <astuurman@hotmail.com>
2023-02-04 12:37:47 +0100

    actions for blocking, grabbing

--- fighter.gd
+++ fighter.gd
@@ -8,6 +8,8 @@ const damage = 10
 var action_left: String
 var action_right: String
 var action_hit: String
+var action_block: String
+var action_grab: String
 
 signal deal_damage(target, damage)
 
@@ -25,19 +27,27 @@ func _ready():
 		action_left = "player1_left"
 		action_right = "player1_right"
 		action_hit = "player1_hit"
+		action_block = "player1_block"
+		action_grab = "player1_grab"
 	elif player_id == 1:
 		action_left = "player2_left"
 		action_right = "player2_right"
 		action_hit = "player2_hit"
+		action_block = "player2_block"
+		action_grab = "player2_grab"
 	else:
 		assert(false, "Invalid player_id")
 		
-func _process(delta):
+func _process(_delta):
 	match animation_state.get_current_node():
 		"idle":
 			if Input.is_action_just_pressed(action_hit):
 				hit_targets.clear()
 				animation_state.travel("punch")
+			elif Input.is_action_just_pressed(action_block):
+				animation_state.travel("block")
+			elif Input.is_action_just_pressed(action_grab):
+				animation_state.travel("grab")
 				
 func _on_fist_hit(area: Area2D) -> void:
 	match animation_state.get_current_node():
