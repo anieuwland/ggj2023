f59669bf98f0e998a2dcfd80ca07ceb75a553c72
Arthur Nieuwland <a.nieuwland@sobolt.com>
2023-02-05 00:04:17 +0100

    No juice in glasses

--- background/glass/glass.gd
+++ background/glass/glass.gd
@@ -7,6 +7,6 @@ func _ready():
 	pass # Replace with function body.
 
 
-#func _process(delta):
-#	juicyness = clamp(juicyness, 0.0, 1.0)
-#	juice.position.y = lerp(0, -37, juicyness)
+func _process(delta):
+	juicyness = clamp(juicyness, 0.0, 1.0)
+	juice.position.y = lerp(809, 392, juicyness)
