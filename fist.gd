8481a9f7182d0abec2de171a1562472f9506342c
Matteo De Carlo <matteo.dek@covolunablu.org>
2023-02-04 19:14:48 +0100

    added faces

--- fist.gd
+++ fist.gd
@@ -2,6 +2,10 @@ extends Area2D
 
 signal hit(area)
 
+onready var smileface: Node2D = $"../../../../../../visuals/smileface"
+onready var happyface: Node2D = $"../../../../../../visuals/happyface"
+onready var animation: AnimationTree = $"../../../../../../animation"
+
 func _ready():
 	connect("area_entered", self, "_on_area_entered")
 	monitoring = false
@@ -10,6 +14,8 @@ func _on_area_entered(area: Area2D) -> void:
 	emit_signal("hit", area)
 
 func _damage_frame(enabled: bool) -> void:
+	smileface.visible = not enabled
+	happyface.visible = enabled
 	if enabled:
 		monitoring = true
 	else:
@@ -23,3 +29,4 @@ func _grab_frame(enabled: bool) -> void:
 		monitoring = true
 	else:
 		monitoring = false
+
