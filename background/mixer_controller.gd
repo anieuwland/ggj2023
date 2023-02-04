9de48c964c71ed330703138b1c8f501e6f7d7c4f
Aart <aart_odding@hotmail.com>
2023-02-04 23:07:20 +0100

    Added most required files for mixer to work

---
+++ background/mixer_controller.gd
@@ -0,0 +1,20 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+export(NodePath) var mixer
+
+onready var mixerNode = get_node(mixer)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	mixerNode.playing = true
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
