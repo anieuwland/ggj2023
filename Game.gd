9dd4b5eb744de972b350bb5cce869ed873decb39
Matteo De Carlo <matteo.dek@covolunablu.org>
2023-02-05 12:53:11 +0100

    Added background

--- Game.gd
+++ Game.gd
@@ -1,14 +1,14 @@
 extends Control
 
 func change_scene(var scene_path: String):
-	while get_child_count() > 0:
-		remove_child(get_child(0))
+	while get_child_count() > 1:
+		remove_child(get_child(1))
 	var new_scene = load(scene_path).instance()
 	add_child(new_scene)
 
 func change_scene_to_node(var scene_node: Node):
-	while get_child_count() > 0:
-		remove_child(get_child(0))
+	while get_child_count() > 1:
+		remove_child(get_child(1))
 	add_child(scene_node)
 
 func _process(delta):
